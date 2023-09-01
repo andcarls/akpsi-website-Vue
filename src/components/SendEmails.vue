495f7c84e07b2923127072ab8c8fdc94e719c645
Andrew Carlson <andcarls@umich.edu>
2023-09-01 19:25:15 -0400

    contnieud'

---
+++ src/components/SendEmails.vue
@@ -0,0 +1,165 @@
+<template>
+    <h1 @click="showSendEmailReminders = !showSendEmailReminders" style="text-decoration: underline;">Send Email
+        Reminders</h1>
+    <div v-if="showSendEmailReminders">
+        <p>Note: you can only send 100 emails per day. Trying to make a request above this number means some emails will be
+            sent, but some lost. Be mindful of how many emails you send.</p>
+        <h2 @click="showAllUserEmail = !showAllUserEmail" style="text-decoration: underline;">Send Reminder to all users
+            (with debts)</h2>
+        <div v-if="showAllUserEmail">
+            <p>Brothers only</p>
+            <button @click="sendToAllWithDebts">Send to all</button>
+            {{ emails }}
+        </div>
+
+    </div>
+</template>
+
+<script>
+import { supabase } from '@/lib/supabase';
+export default {
+    name: 'sendEmails',
+    data() {
+        return {
+            showSendEmailReminders: false,
+            showAllUserEmail: false,
+            emails: {},
+        }
+    },
+    methods: {
+        async sendToAllWithDebts() {
+            const { data, error } = await supabase.from('user_charges')
+                .select(`
+                user_id,
+                amount,
+                due_date,
+                user_information (
+                first_name,
+                email,
+                graduation_year
+            ),
+            events (
+                event_name
+            )
+                `).eq('paid', false);
+            let currDate = new Date();
+            let gradYear;
+            if (currDate.getMonth() > 6) {
+                gradYear = currDate.getFullYear();
+            }
+            else {
+                gradYear = currDate.getFullYear() - 1;
+            }
+            let userCharges = null;
+            if (data && data.length != 0) {
+                userCharges = data.filter(item => item.user_information.graduation_year > gradYear);
+            }
+            console.log(userCharges);
+            if (error) {
+                alert(error.message);
+                return;
+            }
+
+            // 1. get all charges, filter out paid.
+            // 2. iterate through each charge, mapping to unique uuid
+            // 2a. add each charge to object under uuid
+            let chargesByUser = {};
+            let numEmails = 0;
+            userCharges.forEach(charge => {
+                const userId = charge.user_id;
+
+                if (!chargesByUser[userId]) {
+                    chargesByUser[userId] = {
+                        email: charge.user_information.email,
+                        name: charge.user_information.first_name,
+                        charges: []
+                    };
+                    numEmails++;
+                }
+                chargesByUser[userId].charges.push({
+                    event_name: charge.events.event_name,
+                    amount: charge.amount,
+                    due_date: charge.due_date || 'N/A'
+                });
+            });
+            if (!confirm(numEmails + " emails will be sent (daily limit 100)")) {
+                return;
+            }
+            console.log(chargesByUser);
+            for (const userId in chargesByUser) {
+                const user = chargesByUser[userId];
+                const html_body = `
+        <html>
+        <head>
+            <style>
+                body {
+                    font-family: Arial, sans-serif;
+                    margin: 0;
+                    padding: 0;
+                }
+                p {
+                    margin: 0;
+                    padding: 0;
+                }
+                table {
+                    width: 100%;
+                    border-collapse: collapse;
+                    margin-top: 20px;
+                }
+                th, td {
+                    text-align: left;
+                    padding: 8px;
+                }
+                th {
+                    background-color: #f2f2f2;
+                }
+                tr:nth-child(even) {
+                    background-color: #f2f2f2;
+                }
+            </style>
+        </head>
+        <body>
+            <p>Hello ${user.name},</p>
+            <p>You have outstanding debts due to AKPsi-Phi. Please see the details below:</p>
+            <table>
+                <tr>
+                    <th>Event Name</th>
+                    <th>Amount</th>
+                    <th>Due Date</th>
+                </tr>
+                ${user.charges.map(charge => `
+                    <tr>
+                        <td>${charge.event_name}</td>
+                        <td>${charge.amount}</td>
+                        <td>${charge.due_date}</td>
+                    </tr>
+                `).join('')}
+            </table>
+            <p>Thank you for your prompt attention to this matter.</p>
+            <p>Sincerely,<br />VP-Finance</p>
+        </body>
+        </html>
+    `;
+                let message = {
+                    sender: 'AKPsi-Phi VP-Finance <vp.finance@akpsi-phi.com>',
+                    recipient: user.email,
+                    subject: '[ACTION REQUIRED] Outstanding Debts to AKPsi-Phi',
+                    html_body: html_body,
+                };
+                const { data, error } = await supabase.rpc('send_email_message', { message });
+                if (error) {
+                    alert(error.message);
+                    alert('Stopping emails... see messages table on Supabase for error logs and sent emails');
+                    return;
+                }
+                else {
+                    console.log(data);
+                }
+            }
+            alert('emails successfully sent');
+
+        },
+    }
+
+}
+</script>
